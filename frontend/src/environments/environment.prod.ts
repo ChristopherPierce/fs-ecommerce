04fb9c9185f6268b3a7739a47973d6250c3ac43d
ChristopherPierce <78176696+ChristopherPierce@users.noreply.github.com>
2022-05-27 00:38:56 -0500

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  luv2shopApiUrl: "https://ecommerce-back0.azurewebsites.net/api",
+  luv2shopApiUrl: "https://ecom-back.azurewebsites.net/api",
   stripePublishableKey: "pk_test_51KgZckE6okuwhGCfjABpYUUcNI0kbbK36WsnseXatx6xdoSKMsF7hO6mSLHDvNocKnQYdeMI9bwbBCXJpiIKMbM3002aJHIZkX"
 };
