ccaec6988af61bb550103adde935b3738b5e86ea
ChristopherPierce <78176696+ChristopherPierce@users.noreply.github.com>
2022-05-27 00:39:47 -0500

    Update my-app-config.ts

--- frontend/src/app/config/my-app-config.ts
+++ frontend/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa4c8dn9p65fmC7O5d7',
         issuer: 'https://dev-5165573.okta.com/oauth2/default',
-        redirectUri: 'https://ecommerce-front.azurewebsites.net/login/callback',
+        redirectUri: 'https://ecom-app.azurewebsites.net',
         //redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email', 'groups']
     }
